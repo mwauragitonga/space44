3b157163050e8719544b083839f8e32bc7079f3b
Gitonga Mwaura <mwauragitonga12@gmail.com>
2021-12-17 19:20:34 +0300

    sync

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Paper } from '@material-ui/core';
+
 import { makeStyles } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
 import './App.css';
